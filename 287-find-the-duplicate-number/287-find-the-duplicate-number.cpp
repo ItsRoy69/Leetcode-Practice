f816889a529867381a2c8ca231be278f24de6382
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2022-08-19 07:03:00 +0530

    Time: 116 ms (95.17%), Space: 61.7 MB (25.73%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -4,6 +4,7 @@ public:
         int MAX = 1e5+1;
         int count[MAX];
         memset(count,0,sizeof(count));
+        //The memset() function in C++ copies a single character for a specified number of time to an object.
         for(auto &i:nums) {
             count[i]++;
             if(count[i]==2) return i;
