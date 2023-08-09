24e35f3987720f6ed0822cb794c6d8321519d7c1
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2023-08-09 10:00:35 +0530

    Time: 2 ms (21.84%), Space: 39.7 MB (99%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,34 +1,40 @@
 class Solution {
     int[] nums;
     public int splitArray(int[] nums, int m) {
-        this.nums = nums;
-        int low = 0, high = 0, min = Integer.MAX_VALUE;
+         int start = 0;
+        int end = 0;
+
         for (int i = 0; i < nums.length; i++) {
-            low = Math.max(low, nums[i]);
-            high += nums[i];
+            start = Math.max(start, nums[i]); // in the end of the loop this will contain the max item of the array
+            end += nums[i];
         }
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (required_no_of_chunks(mid, m)) {
-                min = Math.min(min, mid);
-                high = mid - 1;
+
+        // binary search
+        while (start < end) {
+            // try for the middle as potential ans
+            int mid = start + (end - start) / 2;
+
+            // calculate how many pieces you can divide this in with this max sum
+            int sum = 0;
+            int pieces = 1;
+            for(int num : nums) {
+                if (sum + num > mid) {
+                    // you cannot add this in this subarray, make new one
+                    // say you add this num in new subarray, then sum = num
+                    sum = num;
+                    pieces++;
+                } else {
+                    sum += num;
+                }
+            }
+
+            if (pieces > m) {
+                start = mid + 1;
             } else {
-                low = mid + 1;
+                end = mid;
             }
+
         }
-        return min;
-    }
-    
-    private boolean required_no_of_chunks(int mid, int m) {
-        int chunks = 0, val = 0;
-        for (int i = 0; i < nums.length; i++) {
-            val += nums[i];
-            if (val > mid) {
-                chunks++;
-                val = nums[i];
-            }
-        }
-        chunks++;  // Count the last chunk
-        return chunks <= m;
+        return end;
     }
 }
