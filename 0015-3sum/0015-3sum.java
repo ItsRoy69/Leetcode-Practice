a245298e0397bab8fb5e45be4f6d9e289bb819c6
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2023-08-16 10:29:27 +0530

    Time: 32 ms (85.78%), Space: 50.3 MB (90.43%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        // Sort the array
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate elements for i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            for (; j < k;) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    // Found a triplet with zero sum
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    // Skip duplicate elements for j
+                    for (j++; j < k && nums[j] == nums[j - 1]; j++) {
+                    }
+
+                    // Skip duplicate elements for k
+                    for (k--; j < k && nums[k] == nums[k + 1]; k--) {
+                    }
+                } else if (sum < 0) {
+                    // Sum is less than zero, increment j to increase the sum
+                    j++;
+                } else {
+                    // Sum is greater than zero, decrement k to decrease the sum
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
+}
