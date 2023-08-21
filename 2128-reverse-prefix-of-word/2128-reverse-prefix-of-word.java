3e71d5b58818d123ffbda563b6a175b9dc54d973
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2023-08-22 00:29:59 +0530

    Time: 0 ms (100%), Space: 40.7 MB (71.86%) - LeetHub

---
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+        if(index == -1) return word;
+        StringBuilder sb = new StringBuilder(word.substring(0,index+1));
+        return sb.reverse().toString()+word.substring(index+1);
+    }
+}    
