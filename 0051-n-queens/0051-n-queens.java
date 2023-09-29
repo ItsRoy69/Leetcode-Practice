6c39d2f3ce4f7522f0b432f93908cafc00d45979
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2023-09-30 03:27:33 +0530

    Time: 3 ms (59.35%), Space: 43.9 MB (63.41%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,74 +1,62 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        
-        char [][] board = new char[n][n];
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board.length; j++) {
-                board[i][j] = '.';
-            }
-        }
-        
-        List<List<String>> ans = new ArrayList<>();
-        queen(board, 0, ans);
-        return ans;
+        List<List<String>> result = new ArrayList<>();
+        boolean[][] board = new boolean[n][n];
+        solveQueens(board, 0, result);
+        return result;
     }
 
-    static void queen(char[][] board, int row, List<List<String>> list) {
-
-        if(row == board.length) {
-
-            list.add(construct(board));
+    private void solveQueens(boolean[][] board, int row, List<List<String>> result) {
+        if (row == board.length) {
+            result.add(generateSolution(board));
             return;
         }
 
         for (int col = 0; col < board.length; col++) {
-            if(isSafe(board, row, col)) {
-                board[row][col] = 'Q';
-                queen(board, row + 1, list);
-                board[row][col] = '.';
+            if (isSafe(board, row, col)) {
+                board[row][col] = true;
+                solveQueens(board, row + 1, result);
+                board[row][col] = false;
             }
         }
     }
 
-    static List<String> construct(char[][] board) {
-
-        List<String> internal = new ArrayList<>();
-        for (int i = 0; i < board.length; i++) {
-            String row = new String(board[i]);
-            internal.add(row);
-        }
-        return internal;
-    }
-
-    static boolean isSafe(char[][] board, int row, int col) {
-
-        //for checking vertical row
+    private boolean isSafe(boolean[][] board, int row, int col) {
+        // Check vertical row
         for (int i = 0; i < row; i++) {
-            if(board[i][col] == 'Q') {
+            if (board[i][col]) {
                 return false;
             }
         }
 
-        //for checking left diagonal
-
+        // Diagonal left
         int maxLeft = Math.min(row, col);
-
         for (int i = 1; i <= maxLeft; i++) {
-            if(board[row - i][col - i] == 'Q') {
+            if (board[row - i][col - i]) {
                 return false;
             }
         }
 
-        //for checking right diagonal
-
-        int maxRight = Math.min(row, board.length - 1 - col);
-
+        // Diagonal right
+        int maxRight = Math.min(row, board.length - col - 1);
         for (int i = 1; i <= maxRight; i++) {
-            if(board[row - i][col + i] == 'Q') {
+            if (board[row - i][col + i]) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    private List<String> generateSolution(boolean[][] board) {
+        List<String> solution = new ArrayList<>();
+        for (boolean[] row : board) {
+            StringBuilder sb = new StringBuilder();
+            for (boolean element : row) {
+                sb.append(element ? 'Q' : '.');
+            }
+            solution.add(sb.toString().trim());
+        }
+        return solution;
     }
 }
